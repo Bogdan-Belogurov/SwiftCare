6f25af00838732994ac071c71455b53d875cb17b
Bogdan Belogurov <bogdan.belogurov@emlid.com>
2023-11-25 15:38:08 +0100

    Initial Commit

---
+++ SwiftCare/SwiftCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCareApp.swift
+//  SwiftCare
+//
+//  Created by Bogdan Belogurov on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
